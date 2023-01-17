889006cbffec7a9fa2ea6e658a86f4ef31d43946
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-17 21:27:01 +0900

    [5] Data class2

--- app/src/main/java/com/hankim/prctice04calculator/dao/HistoryDao.kt
+++ app/src/main/java/com/hankim/prctice04calculator/dao/HistoryDao.kt
@@ -1,8 +1,16 @@
 package com.hankim.prctice04calculator.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.hankim.prctice04calculator.model.History
 
 @Dao
 interface HistoryDao {
 
+    @Query("SELECT * FROM history")
+    fun getAll(): List<History>
+
+    @Insert
+    fun insertHistory(history: History)
 }
